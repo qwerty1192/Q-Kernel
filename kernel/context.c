40ae69a787643f1de03cbb0bc9c6258fa636f2db
qwerty1192 <qwerty1192@users.noreply.github.com>
2023-12-31 10:15:55 +0900

    make-context

---
+++ kernel/context.c
@@ -0,0 +1,18 @@
+#include <typedef.h>
+
+typedef struct {
+    u32 r[13];
+    u32 lr;
+    u32 pc;
+    u32 xpsr;
+} Context;
+
+void* make_context(u32 *stackPointer, UINT ssize, void* (*startp)()){
+    Context *ctx;
+    ctx = (Context*)((u32)stackPointer + ssize);
+    ctx--:
+    ctx->pc = (u32)startp & ~0x00000001UL;
+    ctx->xpsr = 0x01000000;
+
+    return (void*)ctx;
+}
