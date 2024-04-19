392c0b9ca2346e9a1296373bc9c9c71d8bcaad22
qwerty1192 <qwerty1192@users.noreply.github.com>
2024-04-19 23:12:06 +0900

    ppb_base thread用のtypedef

--- inc/kernel.h
+++ inc/kernel.h
@@ -2,35 +2,49 @@
 #define _KERNEL_H_
 
 #include "hardware/regs/m0plus.h"
+#include "hardware/regs/addressmap.h"
 #include "typedef.h"
 
 typedef u32 q_thread_id;
 
-// typedef enum {
-//     THREAD_STATE_CREATED,
-//     THREAD_STATE_RUNNING,
-//     THREAD_STATE_BLOCKED,
-//     THREAD_STATE_TERMINATED
-// } q_thread_state;
+typedef struct {
+    u32 r[13];
+    u32 lr;
+    u32 pc;
+    u32 xpsr;
+} q_thread_context;
 
-// // スレッド制御ブロック
-// typedef struct {
-//     q_thread_id id;         // スレッド識別子
-//     u16 priority;           // スレッド優先度
-//     q_thread_state state;   // スレッド状態
-//     void* stack_pointer;    // スタックポインタ
-//     u32 stack_size;         // スタックサイズ
+typedef enum {
+    Q_THREAD_STATE_DORMANT    = 0,
+    Q_THREAD_STATE_RUNNING    = 1,
+    Q_THREAD_STATE_READY      = 2,
+    Q_THREAD_STATE_WAITING    = 3,
+    Q_THREAD_STATE_TERMINATED = 4
+} q_thread_state;
 
-//     void* (*threadadr);     // スレッド開始アドレス
-// } q_thread_control_block;
+// スレッド制御ブロック
+typedef struct {
+    void *context;          // コンテキスト
+
+    struct q_thread_control_block *next; // 次のスレッド
+    struct q_thread_control_block *prev; // 前のスレッド
+
+    q_thread_id id;         // スレッド識別子
+    q_thread_state state;   // スレッド状態
+    u16 priority;           // スレッド優先度
+    void *stack_pointer;    // スタックポインタ
+    u32 stack_size;         // スタックサイズ
+
+    void* (*threadadr);     // スレッド開始アドレス
+} q_thread_control_block;
 
 // ディスパッチャ呼び出し
 static inline void *dispatch(void){
     // PendSV例外発生
-   *(_u32*)(M0PLUS_ICSR_OFFSET | 0xe0000000) = M0PLUS_ICSR_PENDSVSET_BITS;
+   *(_u32*)(M0PLUS_ICSR_OFFSET | PPB_BASE) = M0PLUS_ICSR_PENDSVSET_BITS;
 }
 
-extern void *init_context(u32 *stackPointer, UINT ssize, void (*startp)());
+extern void *make_context(u32 *stackPointer, UINT ssize, void (*startp)());
 extern void dispatch_entry(void);
 
 #endif
