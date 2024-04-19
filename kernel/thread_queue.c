3d62be3fc5141698329eef0a927392dbc60fbea6
qwerty1192 <qwerty1192@users.noreply.github.com>
2024-04-19 23:05:52 +0900

    thread_queue

--- kernel/thread_queue.c
+++ kernel/thread_queue.c
@@ -2,27 +2,42 @@
 #include "../inc/typedef.h"
 
 void queue_add_thread(q_thread_control_block **queue, q_thread_control_block *thread) {
-    // Add a thread to the thread queue
     if(queue == NULL) {
         *queue = thread;
-        thread->prev = NULL;
+        thread->prev = tread;
     } else {
-        thread->next = queue->next;
-        thread->prev = queue;
-        queue->next->prev = thread;
-        queue->next = thread;
+        q_thread_control_block *queue_end;
+        queue_end = (*queue)->prev;
+        queue_end->next = queue;
+        thread->prev = queue_end;
+        *queue->prev = thread;
     }
     thread->next = NULL;
 }
 
-void queue_remove_thread(q_thread_control_block *thread) {
-    // Remove a thread from the thread queue
-    if(thread->prev != NULL) {
-        thread->prev->next = thread->next;
+void queue_remove_top(q_thread_control_block **queue) {
+    q_thread_control_block *top;
+
+    if(*queue == NULL) return;
+
+    top = *queue;
+    *queue = top->next;
+    if(*queue != NULL) {
+        (*queue)->prev = top->prev;
     }
-    if(thread->next != NULL) {
-        thread->next->prev = thread->prev;
+}
+
+void queue_remove_thread(q_thread_control_block **queue, q_thread_control_block *thread) {
+    if(*queue == NULL) return;
+
+    if(*queue == thread) {
+        queue_remove_top(queue);
+    } else {
+        (thread->prev)->next = thread->next;
+        if(thread->next != NULL) {
+            (thread->next)->prev = thread->prev;
+        } else {
+            (*queue)->prev = thread->prev;
+        }
     }
-    thread->next = NULL;
-    thread->prev = NULL;
 }
